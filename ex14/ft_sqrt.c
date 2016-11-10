0f7a80b9fe15a163531153cccc28a4bec890385f
Michael WILK <mwilk@e3r3p12.42.fr>
2016-11-10 13:35:31 +0100

    HEY THIRD TRY

--- ex14/ft_sqrt.c
+++ ex14/ft_sqrt.c
@@ -6,20 +6,21 @@
 /*   By: mwilk <mwilk@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/08 15:55:21 by mwilk             #+#    #+#             */
-/*   Updated: 2016/11/10 12:58:30 by mwilk            ###   ########.fr       */
+/*   Updated: 2016/11/10 13:34:10 by mwilk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 int		ft_sqrt(int nb)
 {
 	int i;
 
-	i = 0;
-	while ((i * i) > 0 && (i * i) < nb)
-		i++;
-	if ((i * i) == nb)
-		return (i);
+	i = 1;
+	if (nb)
+	{
+		while ((i * i) < nb)
+			++i;
+		if (!(nb % i))
+			return (i);
+	}
 	return (0);
 }
