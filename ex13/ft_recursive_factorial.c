a36c5ef726103aac46351ebd361b05490e9df561
Michael WILK <mwilk@e3r3p12.42.fr>
2016-11-10 13:49:19 +0100

    PENTA TRYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,13 +6,13 @@
 /*   By: mwilk <mwilk@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/08 15:36:22 by mwilk             #+#    #+#             */
-/*   Updated: 2016/11/10 12:57:36 by mwilk            ###   ########.fr       */
+/*   Updated: 2016/11/10 13:48:38 by mwilk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_recursive_factorial(int nb)
 {
-	if (nb > 0 && nb <= 16)
+	if (nb > 0 && nb <= 12)
 		return (nb * ft_recursive_factorial(nb - 1));
 	else if (!nb)
 		return (1);
